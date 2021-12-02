07b3df75d33d382f253e468b789fc0274201c753
Soto Conuecar,Miguel M <m.sotoconuecar@student.fontys.nl>
2021-12-02 11:21:24 +0100

    intial commit gateway project

---
+++ Plantr.Gateway/Controllers/AuthenticationController.cs
@@ -0,0 +1,64 @@
+﻿using authService.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+using Plantr.Gateway.Models;
+
+namespace Plantr.Gateway.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly ILogger<AuthenticationController> _logger;
+
+        public AuthenticationController(ILogger<AuthenticationController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpPost]
+        [Route("/login")]
+        public async Task<String> Login([FromBody] User login)
+        {
+            var json = JsonConvert.SerializeObject(User);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var url = "https://httpbin.org/post";
+            using var client = new HttpClient();
+
+            var response = await client.PostAsync(url, data);
+
+            var respBody = await response.Content.ReadAsStringAsync();
+
+            return respBody;
+
+
+
+
+        }
+
+        [HttpPost]
+        [Route("/register")]
+
+        public IActionResult Register([FromBody] User login)
+        {
+
+
+            return null;
+        }
+
+
+
+
+    }
+}
---
+++ Plantr.Gateway/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace authService.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Text.Json.Serialization;
+
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+        //[JsonIgnore] // does not need to be included in returned API responses
+        public string Password { get; set; }
+
+        public bool Admin { get; set; }
+
+    }
+}
